8bd4d0bd6c8daa7fa2a378f2f340a123f82578b6
OndraS-cz <Ondra.Severa@gmail.com>
2024-08-27 13:04:00 +0200

    Hello, world!

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse('Hello, world!')
+
+
+def hello2(request, s):
+    return HttpResponse(f"Hello, {s} world!!")
