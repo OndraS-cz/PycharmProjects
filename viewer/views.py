a635d408accda0a05f6e3406f46fd81c0995709e
OndraS-cz <Ondra.Severa@gmail.com>
2024-08-28 08:58:19 +0200

    Hello, world!

--- viewer/views.py
+++ viewer/views.py
@@ -10,3 +10,19 @@ def hello(request):
 
 def hello2(request, s):
     return HttpResponse(f"Hello, {s} world!!")
+
+
+def hello3(request):
+    s = request.GET.get('s', '')
+    return HttpResponse(f"Hello, {s} world!!")
+
+
+def add(request, num1, num2):
+    return HttpResponse(f"{num1} + {num2} = {num1 + num2}")
+
+
+def add2(request):
+    num1 = int(request.GET.get('num1'))
+    num2 = int(request.GET.get('num2'))
+    return HttpResponse(f"{num1} + {num2} = {num1 + num2}")
+
